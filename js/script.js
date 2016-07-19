b26753b6ff0d9effeb8f2eb9d743ecad2b378a7b
Kris Pypen <kris.pypen@gmail.com>
2016-07-19 23:31:46 +0200

    website

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* Author:
+
+*/
+jQuery(document).ready(function(e) {
+/*    var h = jQuery(window).innerHeight();
+	var f = jQuery('#frame').innerHeight();
+	var nh = (h-f)/2;
+	jQuery('#frame').css('margin-top',nh+'px');
+*/});
+
+if(jQuery.browser.mobile){
+	//alert('dit is een mobile browser');
+	}
